f1cf8c5a5f244512bcef49662c0f8ebb7b807c01
sgh <8857807@163.com>
2024-05-11 22:52:18 +0800

    修改授权排除ROLE_ANONYMOUS

--- biz/wx-dss/src/main/java/com/xiaosi/wx/handler/WxDssAuthorizationManager.java
+++ biz/wx-dss/src/main/java/com/xiaosi/wx/handler/WxDssAuthorizationManager.java
@@ -16,8 +16,11 @@ import org.springframework.security.web.access.intercept.RequestAuthorizationCon
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -27,15 +30,19 @@ public class WxDssAuthorizationManager implements AuthorizationManager<RequestAu
     private SysMenuMapper menuMapper;
 
     @Override
-    public AuthorizationDecision check(Supplier<Authentication> authentication, RequestAuthorizationContext requestAuthorizationContext) {
+    public AuthorizationDecision check(Supplier<Authentication> auth, RequestAuthorizationContext requestAuthorizationContext) {
+        Authentication authentication = auth.get();
+        if(Objects.isNull(authentication)){
+            return new AuthorizationDecision(false);
+        }
+        Set<String> set = authentication.getAuthorities().stream().map(m -> m.getAuthority()).collect(Collectors.toSet());
+        if(set.size()==1&&set.contains("ROLE_ANONYMOUS")){
+            return new AuthorizationDecision(false);
+        }
+
         //获取请求路径
         HttpServletRequest request = requestAuthorizationContext.getRequest();
         String uri = request.getRequestURI();
-        String url = request.getRequestURL().toString();
-
-        log.info("uri======>>>>>{}",uri);
-        log.info("url======>>>>>{}",url);
-
         if("/login".equals(uri) || "/logout".equals(uri) || "/error".equals(uri)){
         return new AuthorizationDecision(true);
         }
@@ -53,7 +60,7 @@ public class WxDssAuthorizationManager implements AuthorizationManager<RequestAu
             return new AuthorizationDecision(true);
         }
 
-        Collection<? extends GrantedAuthority> authorities = authentication.get().getAuthorities();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 
         for (GrantedAuthority authority : authorities) {
             String userPerm = authority.getAuthority();
