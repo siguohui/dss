199735f0f9f5f0cb9cce4ceefa280c9e463fa649
sgh <8857807@163.com>
2024-10-30 09:02:03 +0800

    stomp-rabbitmq

---
+++ biz/wx-ws/rabbitmq-stomp/src/main/java/org/xiaosi/rabbitmq/stomp/RabbitmqStompApplication.java
@@ -0,0 +1,12 @@
+package org.xiaosi.rabbitmq.stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqStompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqStompApplication.class,args);
+    }
+}
