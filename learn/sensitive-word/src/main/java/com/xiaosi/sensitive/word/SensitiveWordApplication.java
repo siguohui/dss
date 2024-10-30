0f5d217a5c6a55b46861545e251b624b836a0f7b
sgh <8857807@163.com>
2024-10-30 08:58:51 +0800

    过滤敏感词

---
+++ learn/sensitive-word/src/main/java/com/xiaosi/sensitive/word/SensitiveWordApplication.java
@@ -0,0 +1,12 @@
+package com.xiaosi.sensitive.word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensitiveWordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensitiveWordApplication.class,args);
+    }
+}
