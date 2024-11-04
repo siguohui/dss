8fc8d1ea3e83651da55e1725f1e16ece240dcbd1
sgh <8857807@163.com>
2024-11-04 16:11:05 +0800

    DateFormat\NumberFormat

--- biz/wx-ws/stomp/src/main/java/org/xiaosi/stomp/controller/TestController.java
+++ biz/wx-ws/stomp/src/main/java/org/xiaosi/stomp/controller/TestController.java
@@ -1,11 +1,18 @@
 package org.xiaosi.stomp.controller;
 
+import cn.hutool.core.collection.CollUtil;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.xiaosi.stomp.schedule.ScheduleTask;
+
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author wl
@@ -28,4 +35,41 @@ public class TestController {
         scheduleTask.setCron(cron);
         return "ok";
     }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        Set<Integer> collect = list.stream().filter(Objects::nonNull).collect(Collectors.toSet());
+
+        System.out.println(collect.isEmpty());
+
+
+        System.out.println(CollUtil.max(list.stream().filter(Objects::nonNull).collect(Collectors.toSet())));
+
+
+        Locale locale = new Locale("zh","CN");
+        NumberFormat instance = NumberFormat.getCurrencyInstance(locale);
+        double money = 15673.3363;
+        System.out.println(instance.format(money));
+
+        Locale locale1 = new Locale("en","US");
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM,locale1);
+        System.out.println(dateFormat.format(date));
+
+
+        List<String> l = Lists.newArrayList();
+        l.add("张三");
+        l.add("张三");
+        l.add("张三");
+        l.add("李四");
+        l.add("李四");
+        l.add("王五");
+
+        List<String> newList = l.stream().distinct().toList();
+
+        System.out.println(newList);
+    }
+
+
 }
