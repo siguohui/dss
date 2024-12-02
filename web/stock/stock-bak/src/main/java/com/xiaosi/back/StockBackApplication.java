b77f50947799becd0651cad3240d4b524aee6822
sgh <8857807@163.com>
2024-12-02 09:07:36 +0800

    stock

---
+++ web/stock/stock-bak/src/main/java/com/xiaosi/back/StockBackApplication.java
@@ -0,0 +1,12 @@
+package com.xiaosi.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockBackApplication.class,args);
+    }
+}
