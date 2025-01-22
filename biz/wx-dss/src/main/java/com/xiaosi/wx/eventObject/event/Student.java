057324bd2f548e97477d7600fd296e1fa38f28c7
sgh <8857807@163.com>
2025-01-22 17:36:32 +0800

    wx-dss

---
+++ biz/wx-dss/src/main/java/com/xiaosi/wx/eventObject/event/Student.java
@@ -0,0 +1,15 @@
+package com.xiaosi.wx.eventObject.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    private String name;
+}
